7765cde27e0837d82851549f906ca894e6544efe
Kajal jaglan <jaglankajal0000@gmail.com>
2024-12-23 11:43:11 +0530

    Committing pom.xml
    First commit POM.XML

---
+++ cloudcast/src/main/java/com/cloudcast/weather/CloudcastApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcast.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcastApplication.class, args);
+	}
+
+}
---
+++ cloudcast/src/test/java/com/cloudcast/weather/CloudcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcast.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
